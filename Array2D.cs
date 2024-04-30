94fa22ef7abd909c98c29f86d0ea3b2b96e1d8c0
Martynas Antipenkovas <martynas.antipenkovas@hotmail.lt>
2024-04-30 12:25:54 +0300

    Update Array2D.cs
    Added a nice foreach handling that I've found convenient

--- Array2D.cs
+++ Array2D.cs
@@ -1,25 +1,73 @@
-public class Array2D<T>
+using System;
+
+namespace Array2D
 {
-	private T[] _array;
-	public uint MaxX { get; private set; }
-	public uint MaxY { get; private set; }
-	public uint TotalSpace => MaxX * MaxY;
-	private uint GetIndex(uint x, uint y) => x + y * MaxX;
-	public void SetAt(uint x, uint y, T value) =>
-		_array[GetIndex(x, y)] = value;
-	public T GetAt(uint x, uint y) => _array[GetIndex(x, y)];
-	public Array2D(uint maxX, uint maxY)
-	{
-		_array = new T[maxX * maxY];
-		MaxX = maxX;
-		MaxY = maxY;
-	}
-	public void SetAll(T value)
-	{
-		uint maxIndex = MaxX * MaxY;
-		for (int i = 0; i < maxIndex; i++)
-		{
-			_array[i] = value;
-		}
-	}
+    public partial class Array2D<T>
+    {
+        private readonly T[] _array;
+        public int MaxX { get; private set; }
+        public int MaxY { get; private set; }
+        public int TotalSpace => MaxX * MaxY;
+        private int GetIndex(int x, int y) => x + y * MaxX;
+
+        public void SetAt(int x, int y, T value) =>
+            _array[GetIndex(x, y)] = value;
+
+        public T GetAt(int x, int y) => _array[GetIndex(x, y)];
+
+        public Array2D(int maxX, int maxY)
+        {
+            _array = new T[maxX * maxY];
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+
+        public void SetAll(T value)
+        {
+            int maxIndex = MaxX * MaxY;
+            for (int i = 0; i < maxIndex; i++)
+            {
+                _array[i] = value;
+            }
+        }
+
+        public bool IsInBounds(int x, int y) => x >= 0
+            && x < MaxX
+            && y >= 0
+            && y < MaxY;
+
+        public void ForeachValidItem(Action<int, int, T> delegateFunction)
+        {
+            for (int y = 0; y < MaxY; y++)
+            {
+                for (int x = 0; x < MaxX; x++)
+                {
+                    T heldValue = GetAt(x, y);
+                    if (GetAt(x, y) is null)
+                    {
+                        continue;
+                    }
+
+                    delegateFunction(x, y, heldValue);
+                }
+            }
+        }
+
+        public void ForeachValidItem(Func<int, int, bool> condition, Action<int, int, T> delegateFunction)
+        {
+            for (int y = 0; y < MaxY; y++)
+            {
+                for (int x = 0; x < MaxX; x++)
+                {
+                    T heldValue = GetAt(x, y);
+                    if (heldValue is null || condition(x, y))
+                    {
+                        continue;
+                    }
+
+                    delegateFunction(x, y, heldValue);
+                }
+            }
+        }
+    }
 }
